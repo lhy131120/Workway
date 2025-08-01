dff495f825f5f6c2668e56324822e8bcd295ca0d
Ray <lhy131120@gmail.com>
2025-08-01 16:53:56 +0800

    update form page

--- main.js
+++ main.js
@@ -3,4 +3,16 @@ import { Modal } from 'bootstrap';
 
 import './assets/scss/all.scss';
 
-console.log('Hello world');
+document.addEventListener('DOMContentLoaded', () => {
+  document.addEventListener('click', e => {
+    const item = e.target.closest('.dropdown-item');
+    if (item) {
+      e.preventDefault();
+      const selectedOption = item.getAttribute('data-option');
+      const dropdownButton = item.closest('.dropdown').querySelector('.dropdown-toggle');
+      if (dropdownButton) {
+        dropdownButton.textContent = selectedOption;
+      }
+    }
+  });
+});
